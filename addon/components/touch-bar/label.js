10e53d373740d576f80e27bf4b094d2e6b109427
Wang Zhongliang <zwang@rebelworks.io>
2017-02-23 17:39:44 +0800

    Remove redundant tagNames

--- addon/components/touch-bar/label.js
+++ addon/components/touch-bar/label.js
@@ -3,7 +3,6 @@ import TouchBarItem from './-item';
 const { TouchBar } = requireNode('electron');
 
 export default TouchBarItem.extend({
-  tagName: '',
   init() {
     this._super();
     this.set('_touchBarItem', new TouchBar.Label({
